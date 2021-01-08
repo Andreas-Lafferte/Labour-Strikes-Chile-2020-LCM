f1e64f012ea18571ab18de97bcc2ee0ba8bf12da
Andreas-Lafferte <andreas.lafferte.tamayo@gmail.com>
2021-01-08 20:39:31 -0300

    Changes analysis

--- processing/04analysis.R
+++ processing/04analysis.R
@@ -125,7 +125,7 @@ k <-cbind(a1, a2, a3, a4, a5, a6, a7, a8)~1
 
 # ---- 6. Modelos ----
 
-# 6.1. Año 2016 ----
+# 6.1. Año 2016 
 M1<-poLCA(formula = f, data = ohl_2016, nclass = 1, maxiter = 2000, nrep = 1, na.rm = F)
 M2<-poLCA(formula = f, data = ohl_2016, nclass = 2, maxiter = 2000, nrep = 1, na.rm = F)
 M3<-poLCA(formula = f, data = ohl_2016, nclass = 3, maxiter = 2000, nrep = 1, na.rm = F) # este
@@ -138,15 +138,39 @@ M9<-poLCA(formula = f, data = ohl_2016, nclass = 6, maxiter = 2000, nrep = 3, na
 
 # M3 el mejor modelo con 3 clases latentes y ajuste alto/ M9 mejor modelo con 6 clases latentes y ajuste bajo
 
-
 # 6.2. Año 2017
+M1_1<-poLCA(formula = t, data = ohl_2017, nclass = 1, maxiter = 2000, nrep = 1, na.rm = F)
+M2_1<-poLCA(formula = t, data = ohl_2017, nclass = 2, maxiter = 2000, nrep = 1, na.rm = F)
+M3_1<-poLCA(formula = t, data = ohl_2017, nclass = 3, maxiter = 2000, nrep = 1, na.rm = F)
+M4_1<-poLCA(formula = t, data = ohl_2017, nclass = 3, maxiter = 2000, nrep = 2, na.rm = F)
+M5_1<-poLCA(formula = t, data = ohl_2017, nclass = 3, maxiter = 2000, nrep = 3, na.rm = F) # nrep = 1 falso máximo
+M6_1<-poLCA(formula = t, data = ohl_2017, nclass = 4, maxiter = 2000, nrep = 1, na.rm = F) # este 
+M7_1<-poLCA(formula = t, data = ohl_2017, nclass = 4, maxiter = 2000, nrep = 3, na.rm = F)
+M8_1<-poLCA(formula = t, data = ohl_2017, nclass = 5, maxiter = 16000, nrep = 3, na.rm = F)
+M9_1<-poLCA(formula = t, data = ohl_2017, nclass = 6, maxiter = 16000, nrep = 3, na.rm = F)
 
+# M6_1 es el que mejor ajusta con 4 clases latentes/ M8_1 ajusta bien pero con 5 clases latentes
 
 # 6.3. Año 2018 
+M1_2<-poLCA(formula = r, data = ohl_2018, nclass = 1, maxiter = 2000, nrep = 1, na.rm = F)
+M2_2<-poLCA(formula = r, data = ohl_2018, nclass = 2, maxiter = 2000, nrep = 1, na.rm = F)
+M3_2<-poLCA(formula = r, data = ohl_2018, nclass = 3, maxiter = 2000, nrep = 1, na.rm = F)
+M4_2<-poLCA(formula = r, data = ohl_2018, nclass = 3, maxiter = 2000, nrep = 3, na.rm = F) # este
+M5_2<-poLCA(formula = r, data = ohl_2018, nclass = 4, maxiter = 2000, nrep = 3, na.rm = F) # nrep = 1 falso máximo
+M6_2<-poLCA(formula = r, data = ohl_2018, nclass = 5, maxiter = 2000, nrep = 3, na.rm = F)
+M7_2<-poLCA(formula = r, data = ohl_2018, nclass = 6, maxiter = 2000, nrep = 3, na.rm = F)
 
+# M4_2 es el que mejor ajusta con 3 clases latentes/ M6_2 ajusta bien pero con 5 clases latentes
 
 # 6.4. Acumulado 
+M1_3<-poLCA(formula = k, data = ohl_acum, nclass = 1, maxiter = 2000, nrep = 1, na.rm = F)
+M2_3<-poLCA(formula = k, data = ohl_acum, nclass = 2, maxiter = 2000, nrep = 1, na.rm = F)
+M3_3<-poLCA(formula = k, data = ohl_acum, nclass = 3, maxiter = 2000, nrep = 1, na.rm = F) 
+M4_3<-poLCA(formula = k, data = ohl_acum, nclass = 3, maxiter = 16000, nrep = 3, na.rm = F) # este
+M5_3<-poLCA(formula = k, data = ohl_acum, nclass = 4, maxiter = 2000, nrep = 3, na.rm = F) # nrep = 1 falso máximo
+M6_3<-poLCA(formula = k, data = ohl_acum, nclass = 5, maxiter = 2000, nrep = 3, na.rm = F)
 
+# M4_3 es el que mejor ajusta con 3 clases latentes/ M6_3 ajusta bien pero con 5 clases latentes
 
 # ---- 7. Ajuste ---- 
 
@@ -182,6 +206,123 @@ plotdatos2 <- plotdatos[plotdatos$X2=="2",]
 ggplot(plotdatos2, aes(x=L1, y = value, group = X1, colour = X1)) +
   geom_point() + geom_line() + theme_classic() + 
   labs(title = "Gráfico 1. Predicciones de clase año 2016.",
+       x="Variables",
+       y = "Valores",
+       caption = "Fuente: Elaboración propia con base en datos de huelgas laborales OHL-COES (1979-2018)") + 
+  theme(legend.position = "bottom",plot.title = element_text(size=12),
+        plot.subtitle = element_text(size=10),
+        plot.caption = element_text(size=8)) 
+
+# 7.2. Ajuste año 2017 
+M1_1$predcell
+M6_1$predcell
+M8_1$predcell
+
+p1_1<-1-pchisq(M1_1$Chisq, M1_1$resid.df)
+p2_1<-1-pchisq(M2_1$Chisq, M2_1$resid.df)
+p3_1<-1-pchisq(M3_1$Chisq, M3_1$resid.df)
+p4_1<-1-pchisq(M6_1$Chisq, M6_1$resid.df)
+p5_1<-1-pchisq(M8_1$Chisq, M8_1$resid.df)
+
+AjusteM_2017<-data.frame(c("M1", "M2", "M3", "M6", "M8"),
+                         c(M1_1$llik, M2_1$llik, M3_1$llik, M6_1$llik, M8_1$llik),
+                         c(M1_1$Chisq, M2_1$Chisq, M3_1$Chisq, M6_1$Chisq, M8_1$Chisq),
+                         c(M1_1$Gsq, M2_1$Gsq, M3_1$Gsq, M6_1$Gsq, M8_1$Gsq),
+                         c(M1_1$npar, M2_1$npar, M3_1$npar, M6_1$npar, M8_1$npar),
+                         c(M1_1$aic, M2_1$aic, M3_1$aic, M6_1$aic, M8_1$aic),
+                         c(M1_1$bic, M2_1$bic, M3_1$bic, M6_1$bic, M8_1$bic),
+                         c(p1_1, p2_1, p3_1, p4_1, p5_1))
+colnames(AjusteM_2017)<-c("Modelo", "Loglike", "X2", "G2", "DF", "AIC", "BIC", "P-value")
+
+View(AjusteM_2017)
+
+# Gráfico año 2017
+
+plotdatos <- melt(M6_1$probs) # función de reshape que me permite 'dar vuelta' una tabla
+plotdatos2 <- plotdatos[plotdatos$X2=="2",]
+# el probs son las probabilidades condicionales 
+
+ggplot(plotdatos2, aes(x=L1, y = value, group = X1, colour = X1)) +
+  geom_point() + geom_line() + theme_classic() + 
+  labs(title = "Gráfico 1. Predicciones de clase año 2017",
+       x="Variables",
+       y = "Valores",
+       caption = "Fuente: Elaboración propia con base en datos de huelgas laborales OHL-COES (1979-2018)") + 
+  theme(legend.position = "bottom",plot.title = element_text(size=12),
+        plot.subtitle = element_text(size=10),
+        plot.caption = element_text(size=8)) 
+
+# 7.3. Ajuste año 2018 
+M1_2$predcell
+M4_2$predcell
+M6_2$predcell
+
+p1_2<-1-pchisq(M1_2$Chisq, M1_2$resid.df)
+p2_2<-1-pchisq(M2_2$Chisq, M2_2$resid.df)
+p3_2<-1-pchisq(M3_2$Chisq, M3_2$resid.df)
+p4_2<-1-pchisq(M4_2$Chisq, M4_2$resid.df)
+p5_2<-1-pchisq(M6_2$Chisq, M6_2$resid.df)
+
+AjusteM_2018<-data.frame(c("M1", "M2", "M3", "M4", "M6"),
+                         c(M1_2$llik, M2_2$llik, M3_2$llik, M4_2$llik, M6_2$llik),
+                         c(M1_2$Chisq, M2_2$Chisq, M3_2$Chisq, M4_2$Chisq, M6_2$Chisq),
+                         c(M1_2$Gsq, M2_2$Gsq, M3_2$Gsq, M4_2$Gsq, M6_2$Gsq),
+                         c(M1_2$npar, M2_2$npar, M3_2$npar, M4_2$npar, M6_2$npar),
+                         c(M1_2$aic, M2_2$aic, M3_2$aic, M4_2$aic, M6_2$aic),
+                         c(M1_2$bic, M2_2$bic, M3_2$bic, M4_2$bic, M6_2$bic),
+                         c(p1_2, p2_2, p3_2, p4_2, p5_2))
+colnames(AjusteM_2018)<-c("Modelo", "Loglike", "X2", "G2", "DF", "AIC", "BIC", "P-value")
+
+View(AjusteM_2018)
+
+# Gráfico año 2018
+
+plotdatos <- melt(M4_2$probs) # función de reshape que me permite 'dar vuelta' una tabla
+plotdatos2 <- plotdatos[plotdatos$X2=="2",]
+# el probs son las probabilidades condicionales 
+
+ggplot(plotdatos2, aes(x=L1, y = value, group = X1, colour = X1)) +
+  geom_point() + geom_line() + theme_classic() + 
+  labs(title = "Gráfico 1. Predicciones de clase año 2018",
+       x="Variables",
+       y = "Valores",
+       caption = "Fuente: Elaboración propia con base en datos de huelgas laborales OHL-COES (1979-2018)") + 
+  theme(legend.position = "bottom",plot.title = element_text(size=12),
+        plot.subtitle = element_text(size=10),
+        plot.caption = element_text(size=8)) 
+
+# 7.4. Ajuste acumulado
+M1_3$predcell
+M4_3$predcell
+M6_3$predcell
+
+p1_3<-1-pchisq(M1_3$Chisq, M1_3$resid.df)
+p2_3<-1-pchisq(M2_3$Chisq, M2_3$resid.df)
+p3_3<-1-pchisq(M3_3$Chisq, M3_3$resid.df)
+p4_3<-1-pchisq(M4_3$Chisq, M4_3$resid.df)
+p5_3<-1-pchisq(M6_3$Chisq, M6_3$resid.df)
+
+AjusteM_acum<-data.frame(c("M1", "M2", "M3", "M4", "M6"),
+                         c(M1_3$llik, M2_3$llik, M3_3$llik, M4_3$llik, M6_3$llik),
+                         c(M1_3$Chisq, M2_3$Chisq, M3_3$Chisq, M4_3$Chisq, M6_3$Chisq),
+                         c(M1_3$Gsq, M2_3$Gsq, M3_3$Gsq, M4_3$Gsq, M6_3$Gsq),
+                         c(M1_3$npar, M2_3$npar, M3_3$npar, M4_3$npar, M6_3$npar),
+                         c(M1_3$aic, M2_3$aic, M3_3$aic, M4_3$aic, M6_3$aic),
+                         c(M1_3$bic, M2_3$bic, M3_3$bic, M4_3$bic, M6_3$bic),
+                         c(p1_3, p2_3, p3_3, p4_3, p5_3))
+colnames(AjusteM_acum)<-c("Modelo", "Loglike", "X2", "G2", "DF", "AIC", "BIC", "P-value")
+
+View(AjusteM_acum)
+
+# Gráfico acumulado
+
+plotdatos <- melt(M4_3$probs) # función de reshape que me permite 'dar vuelta' una tabla
+plotdatos2 <- plotdatos[plotdatos$X2=="2",]
+# el probs son las probabilidades condicionales 
+
+ggplot(plotdatos2, aes(x=L1, y = value, group = X1, colour = X1)) +
+  geom_point() + geom_line() + theme_classic() + 
+  labs(title = "Gráfico 1. Predicciones de clase 2016 a 2018",
        x="Variables",
        y = "Valores",
        caption = "Fuente: Elaboración propia con base en datos de huelgas laborales OHL-COES (1979-2018)") + 
